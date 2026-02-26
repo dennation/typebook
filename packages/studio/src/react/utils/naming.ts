7d3fb0812ec6e748dd4c7dd4e403807edc3f4042
Denis Goncharenko <mrdenzo@yandex.ru>
2026-02-26 14:30:15 +0600

    fix: replace dots and special characters with hyphens in URL hashes
    toKebabCase now replaces all non-alphanumeric characters with hyphens
    and trims leading/trailing hyphens. Produces clean URLs like
    #hero-ui-checkbox/api instead of #hero-ui.checkbox/api.

--- packages/studio/src/react/utils/naming.ts
+++ packages/studio/src/react/utils/naming.ts
@@ -3,7 +3,9 @@ import type { ComponentEntry } from '../../types.js'
 export function toKebabCase(str: string): string {
 	return str
 		.replace(/([A-Z]+)([A-Z][a-z])/g, '$1-$2')
-		.replace(/([a-z])([A-Z])/g, '$1-$2')
+		.replace(/([a-z\d])([A-Z])/g, '$1-$2')
+		.replace(/[^a-zA-Z\d]+/g, '-')
+		.replace(/^-+|-+$/g, '')
 		.toLowerCase()
 }
 
